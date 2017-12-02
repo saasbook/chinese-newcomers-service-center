eea98783a34384f7e1d77f40edd9c15b1087530a
dsproch <dsproch@berkeley.edu>
2017-12-01 16:10:01 -0800

    fixing views to use new model field names

--- app/models/item.rb
+++ app/models/item.rb
@@ -133,28 +133,53 @@ class Item < ActiveRecord::Base
     end
     
     def self.b_headings_names_and_fields
-        return [["Income Issues", [self.income_issues], 
+        return [["Income Issues", self.income_issues, 
                     [:B_1, :B_2, :B_3, :B_4, :B_5, :B_6, :B_7, :B_8, :B_9, :B_10,
-                     :B_11, :B_12, :B_13, :B_14]], 
-                ["Deduction Issues", [self.deduction_issues], 
+                     :B_11, :B_12, :B_13, :B_14], "check_box_tag"], 
+                ["Deduction Issues", self.deduction_issues, 
                     [:B_15, :B_16, :B_17, :B_18, :B_19, :B_20, :B_21, :B_22, 
-                     :B_23, :B_24, :B_25, :B_26, :B_27]], 
-                ["Credit Issues", [self.credit_issues], 
-                    [:B_28, :B_29, :B_30, :B_31, :B_32, :B_33, :B_34]],
-                ["Status Issues", [self.status_issues], 
+                     :B_23, :B_24, :B_25, :B_26, :B_27], "check_box_tag"], 
+                ["Credit Issues", self.credit_issues, 
+                    [:B_28, :B_29, :B_30, :B_31, :B_32, :B_33, :B_34], "check_box_tag"],
+                ["Status Issues", self.status_issues, 
                     [:B_35, :B_36, :B_37, :B_38, :B_39, :B_40, :B_41, :B_42, 
-                     :B_43, :B_44]],
-                ["Tax Issues", [self.tax_refund_return_statue_limitations_issues],
-                    [:B_45, :B_46, :B_47, :B_48, :B_49, :B_50, :B_51, :B_52]], 
-                ["Penalty Issues", [self.penalty_addition_tax_issues], 
-                    [:B_53, :B_54, :B_55, :B_56]],
-                ["Collection Issues", [self.collection_issues],
-                    [:B_57, :B_58, :B_59, :B_60, :B_61, :B_62]]]
+                     :B_43, :B_44], "check_box_tag"],
+                ["Tax Issues", self.tax_refund_return_statue_limitations_issues,
+                    [:B_45, :B_46, :B_47, :B_48, :B_49, :B_50, :B_51, :B_52], "check_box_tag"], 
+                ["Penalty Issues", self.penalty_addition_tax_issues, 
+                    [:B_53, :B_54, :B_55, :B_56], "check_box_tag"],
+                ["Collection Issues", self.collection_issues,
+                    [:B_57, :B_58, :B_59, :B_60, :B_61, :B_62], "check_box_tag"]]
+    end
+
+    def self.k_headings_names_and_fields
+        return [["Case Inventory", self.k_case_inventory, 
+                    [:K_1A, :K_1B, :K_1C, :K_1D, :K_1E], "check_box_tag"], 
+                ["Accounts Management", self.k_accounts_management, 
+                    [:K_2A, :K_2B, :K_2C, :K_2D], "check_box_tag"], 
+                ["Exams", self.k_exams, 
+                    [:K_2E, :K_2F, :K_2G, :K_2H, :K_2I], "check_box_tag"],
+                ["Collection", self.k_collection, 
+                    [:K_2J, :K_2K, :K_2L, :K_2M, :K_2N], "check_box_tag"],
+                ["Appeals", self.k_appeals,
+                    [:K_2O, :K_2P, :K_2Q, :K_2R, :K_2S, :K_2T], "check_box_tag"], 
+                ["Litigation", self.k_litigation, 
+                    [:K_2U, :K_2V], "check_box_tag"],
+                ["Miscellaneous", self.k_miscellaneous, 
+                    [:K_2W, :K_2X, :K_2Y, :K_2Z, :K_2AA], "check_box_tag"],
+                ["Total", self.k_total, 
+                    [:K_total], "check_box_tag"],
+                ["Additional Case Information", self.k_additional_case_information, 
+                    [:K_3, :K_4, :K_5, :K_6, :K_7, :K_8, :K_9, :K_10], "check_box_tag"],
+                ["Tax Court Activities", self.k_tax_court_activities, 
+                    [:K_12, :K_13, :K_14], "check_box_tag"],
+                ["Closed Case Outcomes", self.k_closed_case_outcomes,
+                    [:K_15A, :K_15B, :K_15C, :K_15D], "check_box_tag"]]
     end
 
 
 
-    def self.k_inventory
+    def self.k_case_inventory
         return ["1A. Beginning case inventory(the number of cases that were worked in the previous year that remained open as of the first day of the reporting period)",
                 "1B. New cases opened during the reporting period",
                 "1C. Total number of cases worked during the reporting period (add lines 1A and 1B)",
@@ -162,7 +187,7 @@ class Item < ActiveRecord::Base
                 "1E. Ending case inventory (the number of cases that remained open as of the last day of the reporting period (subtract line 1D from line 1C))"]
     end
 
-    def self.k_accounts
+    def self.k_accounts_management
         return ["2A. Return Processing",
                 "2B. Penalty Abatement",
                 "2C. Injured Spouse",
@@ -211,7 +236,7 @@ class Item < ActiveRecord::Base
         return ["TOTAL (add items 2A through 2AA) (Must equal value in line 1C)"]
     end
     
-    def self.k_additional
+    def self.k_additional_case_information
         return ["3. The amount in controversy exceeds $50,000 per tax period. (for cases reported on line 3, include an explanation for each in the Program Narrative, Item 2.vii)",
                 "4. The taxpayer's income exceeds 250% of the federal poverty guidelines",
                 "5. Matters worked in more than on IRS function or U.S. court",
@@ -222,13 +247,13 @@ class Item < ActiveRecord::Base
                 "10. State tax matters"]
     end
     
-    def self.k_tax_court
+    def self.k_tax_court_activities
         return ["12. Number of U.S. Tax Court cases worked during the reporting period in which an appearance was entered pursuant to Tax Court Rule 24",
                 "13. Number of U.S. Tax Court cases worked during the reporting period in which the clinic represented the taxpayer, but no appearance was entered",
                 "14. Number of informal consultations in the U.S. Tax Court during the reporting period in which the clinic provided advice to a taxpayer, but not representation"]
     end
     
-    def self.k_closed
+    def self.k_closed_case_outcomes
         return ["15A. Number of cases in which the taxpayer was brought into filing compliance",
                 "15B. Number of cases in which the taxpayer was brought into collection compliance",
                 "15C. Total amount of dollars refunded in cash to taxpayers",
